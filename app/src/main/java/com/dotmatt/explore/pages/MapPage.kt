fb7a118adecee8a8d56f07ef363799ef1f511870
Matt Kiggen <28730291+mattkiggen@users.noreply.github.com>
2023-01-16 16:01:46 +0200

    Add current location

--- app/src/main/java/com/dotmatt/explore/pages/MapPage.kt
+++ app/src/main/java/com/dotmatt/explore/pages/MapPage.kt
@@ -1,38 +1,45 @@
 package com.dotmatt.explore.pages
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
 
+@SuppressLint("MissingPermission")
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MapPage() {
     val permissionState = rememberPermissionState(permission = android.Manifest.permission.ACCESS_FINE_LOCATION)
-    val singapore = LatLng(1.35, 103.87)
+    val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(LocalContext.current)
+    val (currentLocation, setCurrentLocation) = remember { mutableStateOf(LatLng(-33.918861, 18.423300)) }
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(currentLocation, 15f)
     }
 
     if (permissionState.hasPermission) {
+        fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_BALANCED_POWER_ACCURACY, null).addOnSuccessListener {
+            setCurrentLocation(LatLng(it.latitude, it.longitude))
+            cameraPositionState.position = CameraPosition.fromLatLngZoom(currentLocation, 15f)
+        }
+
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            Marker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
+            Circle(center = currentLocation, fillColor = Color.Blue, radius = 10.0)
         }
     } else {
         Button(onClick = { permissionState.launchPermissionRequest() }) {
